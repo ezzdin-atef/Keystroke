f277102a2220bc99b5480ac289292529c196d104
Ezzdin Atef <ezzdin1125@gmail.com>
2020-04-13 08:25:44 +0200

    lines fixing finally

---
+++ dist/js/main.js
@@ -0,0 +1,9 @@
+var body = document.body,
+    html = document.documentElement;
+
+var height = Math.max( body.scrollHeight, body.offsetHeight, 
+                       html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+$(function () {
+    $('.lines').css('height', height);
+});
