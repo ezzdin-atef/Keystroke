6f4cbe74838b6d206b9bc6ec824bc808289478b4
Ezzdin Atef <ezzdin1125@gmail.com>
2020-04-14 07:54:03 +0200

    before final in shaa allah

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,4 +6,23 @@ var height = Math.max( body.scrollHeight, body.offsetHeight,
 
 $(function () {
     $('.lines').css('height', height);
+
+    var btn = $('#button');
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 300) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
+        }
+    });
+
+    btn.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop:0}, '1000');
+    });
+
 });
+
+
+
