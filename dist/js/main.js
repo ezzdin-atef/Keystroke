c24a399b3c812c2b18818f8e26d2ff768660944a
Ezzdin Atef <ezzdin1125@gmail.com>
2020-09-20 23:23:22 +0200

    box-animation

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,10 +6,10 @@ $(function () {
     let body = document.body,
     html = document.documentElement;
 
-    let height = Math.max( body.scrollHeight, body.offsetHeight, 
+    let height1 = Math.max( body.scrollHeight, body.offsetHeight, 
                         html.clientHeight, html.scrollHeight, html.offsetHeight );
 
-    $('.lines').css('height', height);
+    $('.lines').css('height', height1);
 
 
     // To Top Button
@@ -67,8 +67,73 @@ $(function () {
         $(".mobile-menu + nav").toggleClass("mobile-nav");
     });
     
+    /* Store the element in el */
+    let el = document.querySelectorAll('.box-animation');
 
+    /* Get the height and width of the element */
+    const height = el[0].clientHeight;
+    const width = el[0].clientWidth;
 
+    
+
+    /* Define function a */
+    function handleMove(e) {
+        console.log(e.target.closest(".box-animation"));
+        const el = e.target.closest(".box-animation");
+        /*
+            * Get position of mouse cursor
+            * With respect to the element
+            * On mouseover
+            */
+        /* Store the x position */
+        const xVal = e.layerX;
+        /* Store the y position */
+        const yVal = e.layerY;
+        
+        /*
+            * Calculate rotation valuee along the Y-axis
+            * Here the multiplier 20 is to
+            * Control the rotation
+            * You can change the value and see the results
+            */
+        const yRotation = 20 * ((xVal - width / 2) / width);
+        
+        /* Calculate the rotation along the X-axis */
+        const xRotation = -20 * ((yVal - height / 2) / height);
+        
+        /* Generate string for CSS transform property */
+        const string = 'perspective(500px) scale(1) rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg)';
+        
+        /* Apply the calculated transformation */
+        el.style.transform = string;
+        el.style.zIndex = 5;
+    }
+
+    el.forEach(el => {
+
+        el.addEventListener('mousemove', handleMove);
+
+        /* Add listener for mouseout event, remove the rotation */
+        el.addEventListener('mouseout', function(e) {
+            e.target.closest(".box-animation").style.transform = 'perspective(500px) scale(1) rotateX(0) rotateY(0)';
+            e.target.closest(".box-animation").style.zIndex = 5;
+        })
+
+        /* Add listener for mousedown event, to simulate click */
+        el.addEventListener('mousedown', function(e) {
+            e.target.closest(".box-animation").style.transform = 'perspective(500px) scale(1) rotateX(0) rotateY(0)';
+            e.target.closest(".box-animation").style.zIndex = 5;
+        })
+
+        /* Add listener for mouseup, simulate release of mouse click */
+        el.addEventListener('mouseup', function(e) {
+            e.target.closest(".box-animation").style.transform = 'perspective(500px) scale(1) rotateX(0) rotateY(0)';
+            e.target.closest(".box-animation").sstyle.zIndex = 5;
+        })
+    });
+
+    
+      
 
 });
 
